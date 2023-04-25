49376cf7b0326258989d79a6adc6d9649432fb1c
Federico Maita <fedemaita.programador@gmail.com>
2023-04-25 01:57:37 -0300

    Se agrega el modifica la ruta del main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
+alert("Bienvenido/a  " + nombreUsuario + "!")
+
+let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
+alert("Estimado/a  "+nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
+
+
