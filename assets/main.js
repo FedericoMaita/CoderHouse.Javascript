b1d1f9b4717439e7c71b94fe0a353aa8d73b0319
Federico Maita <fedemaita.programador@gmail.com>
2023-05-09 01:11:07 -0300

    se mejora el index y las dos paginas secundarias

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+// let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
+// alert("Bienvenido/a  " + nombreUsuario + "!")
+
+// let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
+// alert("Estimado/a  " + nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
+
+// if (edadUsuario >= 18 ) {
+//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted es mayor de edad.");
+// } else {
+//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted todavia no es mayor de edad.")}
