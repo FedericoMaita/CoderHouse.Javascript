c0e43921f8e4b6f64bab63bd8b6f8e21e56d794a
Federico Maita <fedemaita.programador@gmail.com>
2023-05-16 01:04:03 -0300

    modifico el css y habilito en script solo en el index

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
-// let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
-// alert("Bienvenido/a  " + nombreUsuario + "!")
+let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
+alert("Bienvenido/a  " + nombreUsuario + "!")
 
-// let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
-// alert("Estimado/a  " + nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
+let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
+alert("Estimado/a  " + nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
 
-// if (edadUsuario >= 18 ) {
-//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted es mayor de edad.");
-// } else {
-//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted todavia no es mayor de edad.")}
+if (edadUsuario >= 18 ) {
+    alert("Estimado/a  " + nombreUsuario + " le informamos que usted es mayor de edad.");
+} else {
+    alert("Estimado/a  " + nombreUsuario + " le informamos que usted todavia no es mayor de edad.")}
