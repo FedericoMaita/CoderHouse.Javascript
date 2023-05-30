9e9e806e5b966bc0f1e5b28f60b5fa9a898a3752
Federico Maita <fedemaita.programador@gmail.com>
2023-05-29 23:49:19 -0300

    habilito el main y creo el alert para el simulador

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,22 @@
-let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
-alert("Bienvenido/a  " + nombreUsuario + "!")
+//el simulador de prestamos todavia no esta funcionando
+let botonPaginaPrestamos = document.getElementById("boton-pagina-prestamos");
+botonPaginaPrestamos.addEventListener("click", () => {
+	alert("Estimado usuario/a, esta seccion todavia se encuentra en desarollo. Disculpe las molestias.");
+});
+//este alert esta diseñado para el boton en el body del index 
+let botonPaginaPrestamosIndex = document.getElementById("boton-pagina-prestamos-index");
+botonPaginaPrestamosIndex.addEventListener("click", () => {
+	alert("Estimado usuario/a, esta seccion todavia se encuentra en desarollo. Disculpe las molestias.");
+});
 
-let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
-alert("Estimado/a  " + nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
 
-if (edadUsuario >= 18 ) {
-    alert("Estimado/a  " + nombreUsuario + " le informamos que usted es mayor de edad.");
-} else {
-    alert("Estimado/a  " + nombreUsuario + " le informamos que usted todavia no es mayor de edad.")}
+// let nombreUsuario = (prompt("Antes de continuar, ingrese su nombre por favor "));
+// alert("Bienvenido/a  " + nombreUsuario + "!")
+
+// let edadUsuario = (prompt("A continuacion, ingrese solo el numero de su edad por favor "));
+// alert("Estimado/a  " + nombreUsuario+" usted ha selecionado que tiene " + edadUsuario + " años ")
+
+// if (edadUsuario >= 18 ) {
+//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted es mayor de edad.");
+// } else {
+//     alert("Estimado/a  " + nombreUsuario + " le informamos que usted todavia no es mayor de edad.")}
